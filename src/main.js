c6128e76964ecd58bdf71fd454c6af919ba140f7
ls <2310975964@qq.com>
2019-02-10 11:52:37 +0800

    iconfont to local, vue-router delete mode history, change assetsPublicPath to './'

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import './assets/fonts/iconfont.css';
 
 Vue.use(ElementUI);
 Vue.use(VueAxios, axios);
